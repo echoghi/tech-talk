a9cb9e2f6703aaa9491cd6ebfd7fc4a863bffb41
emilec <emilec@corp.doctor.com>
2018-10-19 13:13:34 -0700

    mdx setup

---
+++ components/code-block.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { UnControlled as CodeMirror } from 'react-codemirror2';
+
+export const CodeBlock = ({ children, options = {}, style, ...props }) => (
+    <div style={{ fontSize: 18, overflow: 'scroll', ...style }}>
+        <CodeMirror
+            value={children}
+            options={{
+                mode: 'jsx',
+                theme: 'night-owl',
+                lineNumbers: false,
+                readOnly: 'nocursor',
+                autoScroll: false,
+                ...options
+            }}
+            {...props}
+        />
+    </div>
+);
