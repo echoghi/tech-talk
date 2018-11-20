d02ee608cc870e3ce65212a31171010bad97553e
emilec <emilec@corp.doctor.com>
2018-11-19 19:02:49 -0800

    load styles

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 if (typeof window !== 'undefined' && typeof window.navigator !== 'undefined') {
+    require('style-loader!css-loader!codemirror/lib/codemirror.css');
+    require('style-loader!css-loader!./cm-night-owl.css');
+    require('style-loader!css-loader!./styles.css');
     require('codemirror/mode/javascript/javascript');
     require('codemirror/mode/jsx/jsx');
 }
