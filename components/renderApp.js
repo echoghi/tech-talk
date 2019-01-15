39f5ed384b205b21242b634bc6617f0432fcbd9a
emilec <emilec@corp.doctor.com>
2019-01-15 00:39:56 -0800

    code tweak

--- components/renderApp.js
+++ components/renderApp.js
@@ -1,4 +1,16 @@
 export const renderApp = `
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { render } from 'react-testing-library';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { DoctorTheme } from '../src/app/components/MUI/DoctorTheme';
+import AppIndex from '../src/app/components/appIndex';
+import { rootReducer } from '../src/app/components/store';
+
 function renderApp({
     route = '/',
     history = createMemoryHistory({ initialEntries: [route] })
--- components/renderPartial.js
+++ components/renderPartial.js
@@ -1,4 +1,13 @@
 export const renderPartial = `
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { render } from 'react-testing-library';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { rootReducer } from '../src/app/components/store';
+
 function renderPartial(
     ui,
     { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {}
